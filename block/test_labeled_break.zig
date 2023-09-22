3885c5035b8164c87115dbccacd46ceb52fe5337
to-na <tonanatz@gmail.com>
2023-09-23 01:38:16 +0900

    labeled block

---
+++ block/test_labeled_break.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "labeled break from labeled block expression" {
+    var y: i32 = 123;
+
+    const x = blk: {
+        y += 1;
+        break :blk y; // labeled block expression can return values
+    };
+    try expect(x == 124);
+    try expect(y == 124);
+}
